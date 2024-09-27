a2ff80cc6b5034a769c82d385fae431e83249a4a
shreeshskatti <shreeshkatti212@gmail.com>
2024-09-27 21:40:26 +0530

    commited1

--- prog1.c
+++ prog1.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void main()
 {
-printf("hello");
-printf("hi");
-printf("shreesh");
+ printf("hello");
+ printf("hi");
+ printf("shreesh"); 
+printf("this is in master branch");
+printf("this is in features branch");
 }
 
